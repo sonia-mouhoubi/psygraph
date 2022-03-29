c7466c22c816cf62bb18a47533eddc70999969f0
lacroix <jerome.lacroix@laplateforme.io>
2022-03-29 10:07:30 +0200

    début du formulaire de connexion.php

---
+++ view/connexion.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>connexion</title>
+</head>
+<body>
+    <header>
+        <!-- require du footer -->
+    </header>
+    <main>
+        <!-- formulaire de connexion -->
+         <form action="">
+             <div>
+                 <label for="login">Votre login </label>
+                 <input type="text"name="login">
+             </div>
+             <div>
+                 <label for="password">Votre password</label>
+                 <input type="password" name="password" id="password">
+
+             </div>
+             <input type="submit" value="Connexion">
+             
+
+
+
+
+         </form>
+
+
+    </main>
+    <footer>
+        <!-- require du footer -->
+    </footer>
+    
+</body>
+</html>
