db4d07b89a041cfd869de5025ee7abb3259cd886
M <sonia.mouhoubi@laplateforme.io>
2022-03-29 09:39:15 +0200

    Projet Psygraph base

---
+++ view/require/header.php
@@ -0,0 +1,19 @@
+<header>
+    <span class="logo"><a href="accueil.php">Accueil</a></span>
+
+    <input type="checkbox" id="burger" hidden>
+    <label class="menu-burger" for="burger">
+        <span id="span1"></span>
+        <span id="span2"></span>
+        <span id="span3"></span>
+    </label>
+
+    <nav>
+        <a href="reservation.php">Réservation</a>
+        <a href="contact.php">Contact</a>
+        <a href="inscription.php">Inscription</a>
+        <a href="profil.php">Profil</a>
+        <a href="admin.php">Admin</a>
+    </nav>
+
+</header>
