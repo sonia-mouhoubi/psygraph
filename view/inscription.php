c5464f8bccb6094e73e7d9192a12a14ca8f339dd
lacroix <jerome.lacroix@laplateforme.io>
2022-03-30 15:24:30 +0200

    depot

--- view/inscription.php
+++ view/inscription.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
     <link rel="stylesheet" href="../assets/styles/styles.css">
-    <!-- <script src="../assets/js/script.js"></script> -->
+    <script src="../assets/js/script.js"></script>
 </head>
 <body>
     <?php require('require/header.php'); ?>
