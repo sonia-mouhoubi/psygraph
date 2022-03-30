270e5e88db08f7e09228a6adf55c45d3d70747a9
M <sonia.mouhoubi@laplateforme.io>
2022-03-30 14:58:36 +0200

    footer

--- view/require/footer.php
+++ view/require/footer.php
@@ -1,2 +1,7 @@
 <footer>
+    <a href="mailto:psygraph.consultant@gmail.com">psygraph.consultant@gmail.com</a>
+    <a href="https://www.linkedin.com/in/st%C3%A9phanie-ledeme-psygraph-399109228/"><img src="../assets/img/logo-linkedin.png" alt=""></a>
+    <p>© 2022</p>
+    <a href="https://www.facebook.com/psygraphconsultant/"><img src="../assets/img/logo-facebook.png" alt=""></a>
+    <a href="https://www.instagram.com/accounts/login/?next=/psygraphsl/"><img src="../assets/img/logo-insta.png" alt=""></a>
 </footer>
