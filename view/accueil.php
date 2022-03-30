5accbd9bdcc549c91eec6ce951e0e0b545aa6358
M <sonia.mouhoubi@laplateforme.io>
2022-03-30 14:59:05 +0200

    accueil

---
+++ view/accueil.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@700&display=swap" rel="stylesheet">
+    <link href="../assets/styles/styles.css" rel="stylesheet">
+    <script src="../assets/js/script.js"></script>
+</head>
+<body>
+    <?php require 'require/header.php'; ?>
+    <main>
+        <section class="a-propos">
+            <h2>Votre psyco-graphologue</h2>
+
+            <div class="photo-profil">            
+                <img src="../assets/img/stephanie-ledeme.jpg" alt="Photo de Stéphanie Ledemé">
+            </div>
+
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore architecto consequuntur rem excepturi sequi ab quis omnis totam numquam, illum sunt libero unde eos! Perspiciatis ipsum cum laborum ex eos.</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, minima maxime dolorem, debitis voluptate quasi animi mollitia ipsa laborum vero eligendi voluptas ut repellat consectetur alias harum excepturi, inventore enim.</p>
+
+                <a href="">Me contacter</a>
+            </div>
+        </section>
+
+        <section class="infograph">
+            <h2>Positiver en toute objectivité</h2>
+            
+            <div>
+                <p>Le geste d’écrire implique l’être tout entier. Lorsque nous écrivons de nombreuses fonctions sont sollicitées simultanément, à la fois sur un mode conscient et sur un mode inconscient. Le corps, l’attention, la respiration, l’émotion, se répercutent dans le test.
+                L’étude graphique révèle différents aspects de l’être...</p>
+
+                <p>Le symbolisme que nous allons utiliser en <a href="https://fr.wikipedia.org/wiki/Graphologie">Graphologie</a> fait appel à ce registre nuancé qui concerne la sensibilité de chacun et la Psychologie. Un outil précieux à votre service …</p>
+
+                <p>Les secteurs où la Graphologie peut être d’une grande utilité sont très nombreux : Domaines professionnels : prévention sociale, industries de l’embauche, des domaines scolaires, policiers, éducatifs, d’orientations professionnelles ; domaines personnels (famille, couple, relations amicales…)</p>
+
+                <p>Imaginez comment la Graphologie peut améliorer concrètement votre vie en vous aidant à vous connaître mieux et à connaître mieux les autres.</p>
+            </div>
+
+            <img src="../assets/svg/infograph2.svg" alt="forme svg">
+        </section>
+
+        <section class="psychographie">
+            <h2>Qu'est ce que la psychographie ?</h2>
+
+            <div>
+                <p>La graphologie est une technique d'observation et d'interprétation d'une écriture manuscrite visant à établir le portrait psychologique de son scripteur.</p>
+                <p>L’analyse graphologique est utilisée dans deux optiques :
+                ● à visée personnelle, pour obtenir un portrait psychologique complet, et étudier les compatibilités caractérielles au sein d’un couple par exemple ;
+                ● à but professionnel, lors d’un recrutement, comme approche pour voir si le candidat correspond au poste proposé en fonction des qualités attendues, ou encore pour l’aide à l’orientation professionnelle en décelant les aptitudes et les goûts du demandeur.</p>
+                <p>Il y a encore peu, il fallait être graphologue pour de venir graphothérapeute. Preuveque ces deux professions sont associées et complémentaires.
+                Pourquoi ? Parce que la graphologie est l’analyse de l’écriture et une science de la psychologie. La graphologie est l’étude de la compréhension de l’écriture comme reflet de la personnalité.</p>
+                <p>Pour rééduquer, il faut avant tout comprendre ce que nous rééduquons: l’écriture. La graphothérapie est une spécialité de la graphologie.
+                La graphologie est l’analyse de l’écriture et la graphothérapie est la rééducation de l’écriture intégrant pleinement la compréhension psychologique de celle-ci.</p>
+            </div>
+            <img src="../assets/img/psychographie.png" alt="">
+        </section>
+
+        <section class="methodes">
+            <h2>Ma méthode</h2>
+
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate error deleniti fugiat voluptatem voluptates molestiae eligendi quidem ratione repellat amet fuga, minima adipisci, omnis dicta perspiciatis autem aperiam doloribus et.</p>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, porro! Consectetur veritatis sapiente blanditiis voluptate iure, hic earum tempora maiores nesciunt quis pariatur provident ea dicta dolor tenetur aliquid reiciendis.</p>
+            </div>
+            <div>
+            <iframe width="1226" height="480" src="https://www.youtube.com/embed/55rWi6X7cPM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </section>
+
+        <section class="prestation">
+            <h2>Prestations</h2>
+            <div>
+                <p>PARTICULIER :<br> - 50% pour les demandeurs d'emploi et sans revenus</p>
+                <p>PROFESSIONNEL :<br> par devis</p>
+                <p>INDIVIDUEL :<br> 60 € pour 50 minutes</p>
+                <p>BILAN COMPLET :<br> 240 €  à 400 €</p>
+            </div>
+            <a href="">Demande de devis</a>
+        </section>
+    </main>
+    <?php require 'require/footer.php'; ?>
+</body>
+</html>
+
+
+
